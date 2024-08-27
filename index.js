a3c02ba1f1710f2f5a3194c289b23d8a5222216f
robbie-ellis <128123325+robbie-ellis@users.noreply.github.com>
2024-08-27 10:01:29 -0700

    Added initial parsing for json data

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .then(response => {
       const dataSet = response;
+      let dates =[];
+      dataSet.forEach((el, index) => {
+        dates[index] = new Date(el.Year, 0, 1, 0, parseInt(el.Time.substring(0, 2)), parseInt(el.Time.substring(3)));
+      });
+      console.log(dates);
     })
     .catch(error => {
       console.error("There was a problem fetching json data", error);
