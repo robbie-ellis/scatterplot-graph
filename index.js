d3fa94c57851abce16dc78fe315119b0a9c8ab32
robbie-ellis <128123325+robbie-ellis@users.noreply.github.com>
2024-08-26 19:17:48 -0700

    Created initial html and added json fetch to index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
- 
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Response for json data was not ok");
+      }
+      return response.json();
+    })
+    .then(response => {
+      const dataSet = response;
+    })
+    .catch(error => {
+      console.error("There was a problem fetching json data", error);
+    });
+});
